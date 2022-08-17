f077a2618874292498ccb8ed6e65372a4c168ab3
Ирина Смирнова <ismirnova83@yandex.ru>
2022-08-17 14:51:37 +0300

    Models (Category, Genre, Title) and validators

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,75 @@
 from django.db import models
 
-# Create your models here.
+from .validators import year_validator
+
+class Category(models.Model):
+    name = models.CharField(
+        verbose_name='Название категории',
+        max_length=256,
+    )
+    slug = models.SlugField(
+        verbose_name='Идентификатор',
+        max_length=50,
+        unique=True,
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        verbose_name='Название жанра',
+        max_length=256,
+    )
+    slug = models.SlugField(
+        verbose_name='Идентификатор',
+        max_length=50,
+        unique=True,
+    )
+
+    class Meta:
+        verbose_name = 'Жанр'
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.TextField(
+        verbose_name='Название произведения',
+        max_length=256,
+        db_index=True,
+    )
+    year = models.IntegerField(
+        verbose_name='Год написания',
+        validators=[year_validator],
+    )
+    description = models.TextField(
+        verbose_name='Краткое описание',
+        null=True,
+        blank=True,
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        blank=True,
+        db_index=True,
+        verbose_name='Жанр',
+    )
+    category = models.ForeignKey(
+        Category,
+        verbose_name='Категория',
+        db_index=True,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = 'Произведение'
+
+    def __str__(self):
+        return self.name
