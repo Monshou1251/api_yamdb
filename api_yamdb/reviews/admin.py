dd0551d5191e3d3fe28ac985b08046b0d290a25a
zvekon.na <zvekov.na@esphere.ru>
2022-08-20 12:57:57 +0300

    Поправлены можели для админки

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -3,29 +3,28 @@ from django.contrib import admin
 from .models import Category, Genre, Title, Review, Comment
 
 
-admin.site.register(Category)
-admin.site.register(Genre)
-admin.site.register(Title)
-admin.site.register(Review)
-admin.site.register(Comment)
-
-
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'slug']
+    list_display = ('id', 'name', 'slug')
 
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ['id', 'review_id', 'text', 'author', 'pub_date']
+    list_display = ('id', 'review_id', 'text', 'author', 'pub_date')
 
 
 class GenreAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'slug']
+    list_display = ('id', 'name', 'slug')
 
 
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title_id', 'text', 'author', 'score', 'pub_date']
+    list_display = ('id', 'title_id', 'text', 'author', 'score', 'pub_date')
 
 
 class TitleAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'year', 'category']
+    list_display = ('id', 'name', 'year', 'category')
 
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Title, TitleAdmin)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Comment, CommentAdmin)
